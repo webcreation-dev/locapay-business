cf3a263365899512ec7cb51ba1b02221b3ea1c94
webcreation-dev <chabelholdo1@gmail.com>
2026-03-21 01:40:18 +0100

    fix

--- index.js
+++ index.js
@@ -359,7 +359,14 @@ if (process.env.CHROME_BIN) {
 
 const client = new Client({
     authStrategy: new LocalAuth(),
-    puppeteer: puppeteerOptions
+    puppeteer: {
+        ...puppeteerOptions,
+        userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+    },
+    webVersionCache: {
+        type: 'remote',
+        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+    }
 });
 
 client.on('qr', (qr) => {
