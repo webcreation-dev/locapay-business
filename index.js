cfd185a116bafb59567295eba5e90829a5ad139f
webcreation-dev <chabelholdo1@gmail.com>
2026-04-07 09:56:36 +0100

    chore: disable webVersionCache by setting type to none

--- index.js
+++ index.js
@@ -1165,7 +1165,7 @@ const client = new Client({
         userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
     },
     webVersionCache: {
-        type: 'local' // Remplacé 'remote' par 'local' pour éviter l'erreur de destruction du contexte
+        type: 'none'
     }
 });
 
